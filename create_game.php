a329b97ec78a9e1ea8f31cfa7e3d22b8ff248bd5
Snarky <snarky@thesnarky.com>
2008-06-28 16:16:20 -0400

    Added maximum and minimum player amounts to game creation screen

--- create_game.php
+++ create_game.php
@@ -65,7 +65,7 @@
         }
         echo "<form method='POST' action='./create_game.php'>\n";
         echo "<h3>Create Game</h3>\n";
-        echo "<label>Game name: </label>\n";
+        echo "<label><span class='error'>*</span>Game name: </label>\n";
         echo "<input type='text' name='game_name' size='60' ";
         if(isset($game_name) || isset($_POST['game_name'])) {
             if(isset($game_name)) {
@@ -77,6 +77,12 @@
         }
         echo "/>\n";
         echo "<br />\n";
+        echo "<label>Minimum Players: </label>\n";
+        echo "<input type='password' size='2' name='minimum' class='submit' />\n";
+        echo "<br />\n";
+        echo "<label>Maximum Players: </label>\n";
+        echo "<input type='password' size='2' name='maximum' class='submit' />\n";
+        echo "<br />\n";
         echo "<label>Game password: </label>\n";
         echo "<input type='password' name='game_pass' class='submit' />\n";
         echo "<br />\n";
@@ -86,6 +92,13 @@
         echo "<input type='submit' value='Create Game' class='submit' />\n";
         echo "</form>\n";
         echo "</div>\n"; //Close create_game_form
+        echo "<div id='create_game_form'>\n";
+        echo "<p>\n";
+        echo "<span class='error'>*</span> - Required field, all others are optional.<br />\n";
+        echo "If a password is entered once, it must be entered both times.<br />\n";
+        echo "If no maximum and/or minimum is given, the game will be open to as many or as few players as we have rolesets for.<br />";
+        echo "</p>\n";
+        echo "</div>\n";
     } else {
         echo "<div id='create_game_form'>\n";
         echo "<p class='error'>You must be <a href='./login.php'>logged in</a> to create a game.</p>\n";
