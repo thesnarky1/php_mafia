36684fda5d978713016ba8a5af60568195f715db
Snarky <snarky@thesnarky.com>
2008-10-09 23:24:33 -0400

    Started adding registration page. Its for invite only at first. Added reg_code table to db

--- register.php
+++ register.php
@@ -3,6 +3,21 @@
     include('./includes/functions.php');
 
     render_header("Thieves Tavern Register");
+
+    if(isset($_GET['email']) && isset($_GET['code'])) {
+        if(isset($_GET['register'])) {
+            //We're registering
+        } else {
+            //Show register form
+            echo "<form id='registration_form'>\n";
+            echo "<input type='submit' value='Register'>\n";
+            echo "</form>\n";
+        }
+    } else {
+        echo "<p class='error'>Sorry, registration is currently limited to invite only. ".
+             "If you received an invitation, please use the link provided by email.</p>\n";
+    }
+
     render_footer();
 
 ?>
