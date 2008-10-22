711363079085bc014f004d320eaab7bfa6b8ea05
Snarky <snarky@thesnarky.com>
2008-10-22 14:10:13 -0400

    Added mysqli_get_many function to return all rows

--- includes/mysqli_functions.php
+++ includes/mysqli_functions.php
@@ -10,5 +10,17 @@
             return false;
         }
     }
-
+    
+    function mysqli_get_many($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result && mysqli_num_rows($result) > 0) {
+            $to_return = array();
+            while($row = mysqli_fetch_array($result)) {
+                $to_return[] = $row;
+            }
+            return $to_return;
+        }
+        return false;
+    }
 ?>
